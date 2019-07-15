a08016a7c878ecd3be7fd20f975952a2392748e6
JingchaoWei <40045699+JingchaoWei@users.noreply.github.com>
2019-07-15 12:25:28 +0800

    1

--- old/BGI.R
+++ old/BGI.R
@@ -4,8 +4,8 @@
 background_DEG <- read.csv('DEG.csv',stringsAsFactors = F,check.names = T)
 background_all <- read.csv('all.csv',stringsAsFactors = F,check.names = T)
 attach(background_DEG)
-upgenes <- background_DEG[FK.FPKM>=1&HN.FPKM>=1&log2.HN.FK.>=2&Qvalue.FK.vs.HN.<0.01,]
-downgenes <- background_DEG[FK.FPKM>=1&HN.FPKM>=1&log2.HN.FK.<=-2&Qvalue.FK.vs.HN.<0.01,]
+upgenes <- background_DEG[FK.FPKM>=1&HN.FPKM>=1&log2.HN.FK.>=2&Qvalue.FK.vs.HN.<0.001,]
+downgenes <- background_DEG[FK.FPKM>=1&HN.FPKM>=1&log2.HN.FK.<=-2&Qvalue.FK.vs.HN.<0.001,]
 detach(background_DEG)
 save(background_DEG,upgenes,downgenes,background_all,file = 'Data.Rdata')
 
